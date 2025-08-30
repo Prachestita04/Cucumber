d1012507771d38dee989254b5e09821ec8ecf4c3
Prachestita04 <prachestitamahapatra@gmail.com>
2025-08-30 10:39:32 +0530

    Uber feature program in cucumber

--- src/test/java/StepDefinitions/UberStep.java
+++ src/test/java/StepDefinitions/UberStep.java
@@ -9,23 +9,27 @@ public class UberStep {
 
     @Given("User wants to select a car of type {string}")
     public void user_wants_to_select_a_car_of_type(String carType) {
-        System.out.println("Step 1: User wants to select a car of type "+carType);
+        System.out.println("Step 1: User wants to select a car of type " + carType);
     }
+
     @When("User selects car type as {string} and pick up point as {string} and drop point as {string}")
     public void user_selects_car_type_as_and_pick_up_point_as_and_drop_point_as(String carType, String pick, String drop) {
-        System.out.println("Step 2: User selects a car type as "+carType+" "+
-                "add pickup location as "+pick+ " add drop location as "+drop);
+        System.out.println("Step 2: User selects a car type as " + carType + " " +
+                "add pickup location as " + pick + " add drop location as " + drop);
     }
+
     @Then("Driver starts journey")
     public void driver_starts_journey() {
         System.out.println("Step 3: Driver starts journey");
     }
+
     @And("Driver ends journey")
     public void driver_ends_journey() {
         System.out.println("Step 4: Driver ends journey");
     }
+
     @Then("User pays Rs. {int}")
     public void user_pays_rs(Integer fare) {
-        System.out.println("Step 5: User pays the amount of INR "+fare);
+        System.out.println("Step 5: User pays the amount of INR " + fare);
     }
 }
--- src/test/resources/Appfeatures/uberapp.feature
+++ src/test/resources/Appfeatures/uberapp.feature
@@ -1,4 +1,5 @@
 Feature: Uber booking feature
+
   Scenario: Booking cab
     Given User wants to select a car of type "Sedan"
     When User selects car type as "Sedan" and pick up point as "Banglore" and drop point as "Pune"
